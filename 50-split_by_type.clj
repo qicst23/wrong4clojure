d4827c66e6194bf3da69fe9601a9729fd58bfc75
Solomon White <rubysolo@gmail.com>
2013-05-22 09:58:12 -0600

    my solution for #50

--- 50-split_by_type.clj
+++ 50-split_by_type.clj
@@ -1,1 +1,8 @@
 ;; http://www.4clojure.com/problem/50
+
+;; the general idea is to construct a hash-map, keyed by type,
+;; then just extract the values of the hash-map as the solution
+(fn [v]
+  (map vec
+       (map reverse
+            (vals (reduce (fn [m k] (update-in m [(type k)] conj k)) {} v)))))
